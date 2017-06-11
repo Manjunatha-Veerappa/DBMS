f197b024e5077531d124464b6b9cf33604d9b0ca
Manjunatha Veerappa <manjunath.manju.v@gmail.com>
2017-06-12 00:31:55 +0200

    Easter Date Calculation python script

--- EasterDateCalculation.py
+++ EasterDateCalculation.py
@@ -4,8 +4,8 @@ import csv
 
 class EasterDateCalculation:
     def easterDatCalc(self, year):
-        good_friday_date = easter(year) - timedelta(2)
-        easter_monday_date = easter(year) + timedelta(1)
+        good_friday_date = easter(year) - timedelta(2)        # easter(year) gives the easter sunday date - 2 days = good Friday
+        easter_monday_date = easter(year) + timedelta(1)      # easter(year) + 1 day = Easter Monday
         print("good_friday_date: ", good_friday_date)
         print("easter_monday_date: ", easter_monday_date)
 
